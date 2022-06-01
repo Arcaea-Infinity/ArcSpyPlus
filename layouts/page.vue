60bf17c3f3c812a4c6884eda63e3ff8285342076
SngBlog <59474029+SngBlog@users.noreply.github.com>
2022-06-01 22:05:35 +0800

    Extract functional components

---
+++ layouts/page.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <div class="mainPage">
+        <slot ></slot>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, toRefs, onBeforeMount, onMounted } from 'vue'
+</script>
+<style lang="scss" scoped>
+.mainPage {
+    position: relative;
+    background: url("@/assets/img/mainPage_bg.png");
+    background-position: bottom 91% right 56%;
+    background-repeat: no-repeat;
+    background-size: 190%;
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+}
+</style>
