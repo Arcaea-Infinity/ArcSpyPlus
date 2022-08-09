a7959546adaaffa30b944c67d473351f8c1f804a
SngBlog <59474029+SngBlog@users.noreply.github.com>
2022-08-09 11:39:57 +0800

    End:RC.6

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,14 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    // server:{
+
+    // },
+
     vite: {
+        server: {
+            port: 3999
+        },
         esbuild: {
             pure: ["console.log"],
             minify: true
